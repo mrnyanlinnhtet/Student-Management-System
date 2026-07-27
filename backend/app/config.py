33266a8df1852cfd1ff6804e6af075e18d7b101f
Mr.Nyan Linn Htet <81674134+mrnyanlinnhtet@users.noreply.github.com>
2026-07-28 01:13:49 +0630

    Update config.py
    update-config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,8 +3,8 @@ from pydantic_settings import  BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DB_URL: str
     SECRET_KEY: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ALGORITHM: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="allow"
