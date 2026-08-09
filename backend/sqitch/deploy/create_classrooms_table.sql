9c232185647fe043cde2018897f6d7f7fe800114
Mr.Nyan Linn Htet <mr.nyanlinnhtet13@gmail.com>
2026-08-10 01:17:34 +0630

    adding unique

--- backend/sqitch/deploy/create_classrooms_table.sql
+++ backend/sqitch/deploy/create_classrooms_table.sql
@@ -7,8 +7,8 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE TABLE IF NOT EXISTS classrooms (
  id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
  name VARCHAR(100) NOT NULL,
- room_number VARCHAR(100) NOT NULL,
- building_number VARCHAR(100),
+ room_number VARCHAR(100) NOT NULL UNIQUE,
+ building_number VARCHAR(100) UNIQUE,
  capacity INT NOT NULL,
  status_id UUID NOT NULL,
  type_id UUID NOT NULL,
